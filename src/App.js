3d75c179b25fa8e8624553d569f216a6b52f5f28
Ananya Pal <ananya.pal@iemlabs.com>
2024-01-17 11:19:58 +0530

    text added - fully done yahooooooo

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import "./App.css"
 
 function App() {
   return (
-    /*
+    
     <div className='container'>
       <div className='header'><h5>Header</h5></div>
       <div className='hero'><h5>Hero</h5></div>
       <div className='content'><h5>Content</h5></div>
       <div className='sidebar'><h5>Sidebar</h5></div>
       <div className='footer'><h5>Footer</h5></div>
-    </div> */
-    
+    </div> 
+    /*
     <div className='container'>
       <div className='header'></div>
       <div className='hero'></div>
       <div className='content'></div>
       <div className='sidebar'></div>
       <div className='footer'></div>
-  </div>
+  </div>*/
   )
 }
 
